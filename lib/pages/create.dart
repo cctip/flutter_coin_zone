5df69fb4e5b6a84ac8443831162cad4e3c7e2614
grant <grant_chin_gc@proton.me>
2025-04-01 13:32:50 +0800

    feat: update

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -8,90 +8,378 @@ class CreateView extends StatefulWidget {
 }
 
 class CreateViewState extends State<CreateView> {
+  int curTab = 0;
+  List<Color> colorList = [
+    Color(0xFF5900CE),
+    Color(0xFF00E09E),
+    Color(0xFF4BC9FF),
+    Color(0xFFFFA1FB),
+    Color(0xFFFF434E),
+    Color(0xFFFF7D28),
+    Color(0xFF54E500)
+  ];
+  int curColor = 0;
+  double curBlur = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF2F3F4),
       body: Column(
         children: [
-          Container(
-            height: MediaQuery.of(context).padding.top + 64,
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  left: 16,
-                  child: InkWell(
-                    child: Image.asset('assets/icons/close.png', width: 32),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                ),
-                Center(
-                  child: Text('NFT Creation', style: TextStyle(color: Color(0xFF1B191C), fontSize: 18, fontWeight: FontWeight.w500))
-                ),
-                Positioned(
-                  right: 16,
-                  child: Container(
-                    width: 67,
-                    height: 28,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFEDEFF1),
-                      borderRadius: BorderRadius.circular(16)
-                    ),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(0),
-                        foregroundColor: Colors.white,
-                        backgroundColor: Color(0xFF0C0C0D),
-                        disabledForegroundColor: Color.fromRGBO(27, 25, 28, 0.3),
-                        disabledBackgroundColor: Color(0xFFEDEFF1),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                      ),
-                      onPressed: () {},
-                      child: Text('Save', style: TextStyle(fontSize: 16))
-                    ),
-                  )
-                ),
-              ],
-            ),
-          ),
+          header(),
           Expanded(
             child: Container(
               padding: EdgeInsets.all(16),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFF5900CE),
-                  borderRadius: BorderRadius.circular(40)
+              child: createPanel(),
+            )
+          ),
+          controlPanel()
+        ],
+      )
+    );
+  }
+
+  // 头部
+  Widget header() {
+    return Container(
+      height: MediaQuery.of(context).padding.top + 64,
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            left: 16,
+            child: InkWell(
+              child: Image.asset('assets/icons/close.png', width: 32),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ),
+          Center(
+            child: Text('NFT Creation', style: TextStyle(color: Color(0xFF1B191C), fontSize: 18, fontWeight: FontWeight.w500))
+          ),
+          Positioned(
+            right: 16,
+            child: Container(
+              width: 67,
+              height: 28,
+              decoration: BoxDecoration(
+                color: Color(0xFFEDEFF1),
+                borderRadius: BorderRadius.circular(16)
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(0),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color(0xFF0C0C0D),
+                  disabledForegroundColor: Color.fromRGBO(27, 25, 28, 0.3),
+                  disabledBackgroundColor: Color(0xFFEDEFF1),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
                 ),
+                onPressed: () {},
+                child: Text('Save', style: TextStyle(fontSize: 16))
               ),
             )
           ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 16, 16, MediaQuery.of(context).padding.bottom + 16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(32), topRight: Radius.circular(32))
-            ),
-            child: Column(
-              children: [
-                Row(
+        ],
+      ),
+    );
+  }
+
+  // 绘制面板
+  Widget createPanel() {
+    return Container(
+      decoration: BoxDecoration(
+        color: colorList[curColor],
+        borderRadius: BorderRadius.circular(40)
+      ),
+    );
+  }
+
+  // 操作面板
+  Widget controlPanel() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 16, 16, MediaQuery.of(context).padding.bottom + 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(32), topRight: Radius.circular(32))
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                height: 64,
+                width: MediaQuery.of(context).size.width - 32,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Container(height: 64,color: Color(0xFFF2F3F4), width: MediaQuery.of(context).size.width - 32)
+                    tabItem(0, 'Colors'),
+                    tabItem(1, 'Stickers'),
+                    tabItem(2, 'Text'),
+                    tabItem(3, 'Vague'),
                   ],
                 ),
-                SizedBox(height: 16),
-                Container(
-                  height: 80,
-                  color: Color(0xFFF2F3F4),
-                )
+              )
+            ],
+          ),
+          SizedBox(height: 16),
+          SizedBox(
+            child: IndexedStack(
+              index: curTab,
+              children: [
+                colorsBox(),
+                stickersBox(),
+                textBox(),
+                vagueBox()
               ],
             ),
           )
         ],
-      )
+      ),
     );
+  }
+
+  // 切换操作tab
+  Widget tabItem(int index, String name) {
+    final opt = {
+      'Colors': 'colors',
+      'Stickers': 'sticker',
+      'Text': 'text',
+      'Vague': 'blur',
+    };
+    return Expanded(child: InkWell(
+      onTap: (){
+        setState(() {
+          curTab = index;
+        });
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IndexedStack(
+            index: curTab == index ? 1 : 0,
+            children: [
+              Image.asset('assets/images/create/${opt[name]}.png', width: 24),
+              Image.asset('assets/images/create/${opt[name]}_active.png', width: 26),
+            ],
+          ),
+          SizedBox(width: 4),
+          Text(name, style: TextStyle(
+            color: Color(curTab == index ? 0xFF5900CE : 0xFF1B191C),
+            fontSize: curTab == index ? 16 : 14,
+            fontWeight: curTab == index ? FontWeight.w500 : FontWeight.w400
+          ))
+        ],
+      )
+    ));
+  }
+
+  // Colors
+  Widget colorsBox() {
+    Widget colorItem(int index) {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            curColor = index;
+          });
+        },
+        child: Container(
+          width: 64,
+          height: 64,
+          padding: EdgeInsets.all(4),
+          margin: EdgeInsets.only(left: 4),
+          decoration: BoxDecoration(
+            border: Border.all(color: curColor == index ? colorList[index] : Colors.transparent),
+            borderRadius: BorderRadius.circular(16)
+          ),
+          child: Container(decoration: BoxDecoration(color: colorList[index], borderRadius: BorderRadius.circular(12))),
+        ),
+      );
+    }
+    
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 64,
+                height: 64,
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Image.asset('assets/images/create/photo.png', width: 56)
+              ),
+              ...List.generate(colorList.length, (index) => colorItem(index)),
+            ],
+          ),
+        );
+      }
+    );
+  }
+  // Stickers
+  Widget stickersBox() {
+    List<Image> stickerList = [
+      Image.asset('assets/images/stickers/sticker_2.png'),
+      Image.asset('assets/images/stickers/sticker_4.png'),
+      Image.asset('assets/images/NFTs/nft_1.png'),
+      Image.asset('assets/images/NFTs/nft_9.png'),
+    ];
+    Widget __stickerItem(Image image) {
+      return Container(
+        width: 68,
+        height: 68,
+        padding: EdgeInsets.all(6),
+        margin: EdgeInsets.only(left: 8),
+        decoration: BoxDecoration(
+          color: Color(0xFFF2F3F4),
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: image
+      );
+    }
+
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 68,
+                height: 68,
+                padding: EdgeInsets.all(18),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF2F3F4),
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Image.asset('assets/images/create/add.png', width: 32)
+              ),
+              ...List.generate(stickerList.length, (index) => __stickerItem(stickerList[index])),
+            ],
+          ),
+        );
+      }
+    );
+  }
+  // Text
+  Widget textBox() {
+    return Container(
+      padding: EdgeInsets.only(top: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              style: TextStyle(height: 1),
+              decoration: InputDecoration(
+                isDense: true,
+                contentPadding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                hintText: 'Enter text',
+                hintStyle: TextStyle(color: Color.fromRGBO(45, 42, 47, 0.3)),
+                focusedBorder: OutlineInputBorder( // 聚焦状态边框
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                filled: true,
+                fillColor: Color(0xFFF2F3F4),
+              ),
+            )
+          ),
+          SizedBox(width: 16),
+          InkWell(
+            child: Image.asset('assets/images/create/check.png', width: 24),
+          )
+        ],
+      ),
+    );
+  }
+  // Vague
+  Widget vagueBox() {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Blur', style: TextStyle(color: Color(0xff2D2A2F))),
+              Text('${(curBlur * 100).toStringAsFixed(0)}%', style: TextStyle(fontSize: 16))
+            ],
+          ),
+        ),
+        SliderTheme(
+          data: SliderThemeData(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            trackHeight: 12,
+            thumbShape: _CustomThumbShape(),
+          ),
+          child: Slider(
+            value: curBlur,
+            min: 0.0,
+            max: 1.0,
+            divisions: 100,
+            label: (curBlur * 100).toStringAsFixed(0),
+            onChanged: (value) {
+              setState(() {
+                curBlur = value;
+              });
+            },
+            activeColor: Color(0xFF0C0C0D),
+            inactiveColor: Color.fromRGBO(12, 12, 13, 0.3),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// 核心自定义滑块形状
+class _CustomThumbShape extends SliderComponentShape {
+  final double thumbRadius = 8; // 滑块半径
+  final double dotRadius = 4;    // 白点半径
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(thumbRadius);
+  }
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {
+    final Canvas canvas = context.canvas;
+
+    // 1. 绘制蓝色外圆
+    final Paint outerPaint = Paint()
+      ..color = sliderTheme.activeTrackColor! // 使用主题颜色
+      ..style = PaintingStyle.fill;
+
+    canvas.drawCircle(center, thumbRadius, outerPaint);
+
+    // 2. 绘制白色中心点
+    final Paint dotPaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    canvas.drawCircle(center, dotRadius, dotPaint);
   }
 }
