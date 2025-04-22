efc2e3811f071bb14fb2bfbaae63bc2e5952023b
grant <grant_chin_gc@proton.me>
2025-04-22 17:03:52 +0800

    feat: update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ void main() => SharePref.init().then((e) => runApp(MyApp()));
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+//
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
